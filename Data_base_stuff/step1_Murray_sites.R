e6a0bdacd434c34788ace47ce9ee2e5970c35fc5
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-12-16 15:59:11 +1030

    all done need to check if it worked some plots etc...

--- Data_base_stuff/step1_Murray_sites.R
+++ Data_base_stuff/step1_Murray_sites.R
@@ -18,15 +18,21 @@ library(stringr)
 ### Ouyen has two sets of trials. I need to merge the data into one file.
 Ouyen_drill <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/Ouyen.xlsm", 
                     sheet = "Database_format_drill", skip = 1)
-Ouyen_drill <- Ouyen %>% dplyr::select("site":"comments"   )
-Ouyen_spade <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/Ouyen.xlsm", 
-                          sheet = "Database_format_drill", skip = 1)
-names(Ouyen)
+Ouyen_drill <- Ouyen_drill %>% dplyr::select("ID":"comments"   )
 
+Ouyen_spade <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/Ouyen.xlsm", 
+                          sheet = "Database_format_spade", skip = 1)
+
+Ouyen_spade <- Ouyen_spade %>% dplyr::select("ID":"comments"   )
+
+Ouyen_drill<- Ouyen_drill %>% 
+  mutate(site_sub = "Ouyen_drill")
+Ouyen_spade<- Ouyen_drill %>% 
+  mutate(site_sub = "Ouyen_spade")
 
 Ouyen <- rbind(Ouyen_drill, Ouyen_spade)
-
-Ouyen <- Ouyen %>% dplyr::select("site":"comments"   )
+names(Ouyen_spade)
+names(Ouyen_drill)
 
 rm(Ouyen_drill,Ouyen_spade)
 
@@ -40,7 +46,7 @@ Ouyen_metadata <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database
 Lowalide <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/Lowaldie.xlsx",
                           sheet = "Database format", skip = 1)
 names(Lowalide)
-Lowalide <- Lowalide %>% dplyr::select("site":"comments"   )
+Lowalide <- Lowalide %>% dplyr::select("ID":"comments"   )
 
 ### Lowalide metadata
 
@@ -51,8 +57,8 @@ Lowalide_metadata <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_datab
 ### Waikerie 
 Waikerie <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/Waikerie.xlsx",
                        sheet = "Database_format", skip = 1)
-names(Waikerie)
-Waikerie <- Waikerie %>% dplyr::select("site":"comments"   )
+
+Waikerie <- Waikerie %>% dplyr::select("ID":"comments"   )
 
 ### Waikerie metadata
 
@@ -62,8 +68,8 @@ Waikerie_metadata <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_datab
 ### Carwarp 
 Carwarp <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/CarwarpAmelioration.xlsx",
                        sheet = "Database_format", skip = 1)
-names(Carwarp)
-Carwarp <- Carwarp %>% dplyr::select("site":"comments"   )
+
+Carwarp <- Carwarp %>% dplyr::select("ID":"comments"   )
 
 ### Carwarp metadata
 
@@ -75,7 +81,11 @@ Carwarp_metadata <- read_excel("X:/Therese_Jackie/Sandy_soils/Development_databa
 ############################################################################################################################################
 
 
-Murryays_sites <- rbind(Carwarp, Waikerie, Lowalide, Ouyen)
+Murryays_sites <- rbind(Carwarp, Waikerie, Lowalide)
+Murryays_sites <- Murryays_sites %>% 
+  mutate(site_sub = site)
+
+Murryays_sites <- rbind(Murryays_sites, Ouyen)
 
 
 rm(Carwarp,Waikerie,Lowalide, Ouyen )
@@ -103,8 +113,12 @@ Carwarp_soils_constraints <- read_excel("X:/Therese_Jackie/Sandy_soils/Developme
 
 Murrays_sites_soil_constraints <- rbind(Ouyen_soils_constraints, Lowalide_soils_constraints, 
                                         Waikerie_soils_constraints, Carwarp_soils_constraints)
+
+rm(Ouyen_soils_constraints, Lowalide_soils_constraints, 
+   Waikerie_soils_constraints, Carwarp_soils_constraints)
+
+#### write out the file
 write.csv(Murrays_sites_soil_constraints,
           "X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/Murrays_sites_soil_constraints.csv" ,
           row.names = FALSE)
-rm(Ouyen_soils_constraints, Lowalide_soils_constraints, 
-   Waikerie_soils_constraints, Carwarp_soils_constraints)
+
