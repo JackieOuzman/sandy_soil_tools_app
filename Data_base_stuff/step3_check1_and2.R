2f1dc50a171af58fb81802827f080719926b72eb
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-12-17 07:37:36 +1030

    plot step 1 nad 2 checking

---
+++ Data_base_stuff/step3_check1_and2.R
@@ -0,0 +1,7 @@
+
+
+
+
+
+"X:\Therese_Jackie\Sandy_soils\Development_database\other_sites_working\Murrays_sites_step1_2_neat.csv"
+"X:\Therese_Jackie\Sandy_soils\Development_database\other_sites_working\Murrays_sites_step1_2_control.csv"
