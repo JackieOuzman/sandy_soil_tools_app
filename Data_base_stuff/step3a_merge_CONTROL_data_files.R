e678a0b315d84f7d9743179bfcfc721fc66f6cf7
Jackie Ouzman <jackie.ouzman@csiro.au>
2022-02-08 10:42:23 +1030

    merge the control files

---
+++ Data_base_stuff/step3a_merge_CONTROL_data_files.R
@@ -0,0 +1,64 @@
+### merge all the CONTROL data
+
+
+library(readxl)
+library(tidyverse)
+
+#################################################################################################################
+####   Get the data #####
+
+current.folder <- "X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/step2"
+# find the files that you want
+
+list.of.files <- list.files(current.folder, "control.csv",full.names=T) 
+
+list.of.files
+
+
+
+
+
+
+list2env(
+  lapply(setNames(list.of.files, 
+                  make.names(gsub("X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/step2/",  "", list.of.files))),
+         read.csv), envir = .GlobalEnv)
+
+
+
+
+
+sites_merged_control <- rbind(
+  Brimpton.Lake_sites_step1_2_control.csv,
+  Brooker_sites_step1_2_control.csv   ,
+  Bute_sites_step1_2_control.csv,
+  Cadgee_sites_step1_2_control.csv  ,
+  Carwarp_sites_step1_2_control.csv ,
+  Karoonda_sites_step1_2_control.csv ,
+  Lowaldie_sites_step1_2_control.csv ,
+  Murlong_sites_step1_2_control.csv ,
+  Ouyen_spade_sites_step1_2_control.csv  ,
+  Waikerie_sites_step1_2_control.csv,
+  Yenda_sites_step1_2_control.csv,
+  YoungHusband_sites_step1_2_control.csv
+)
+
+
+
+sites_merged_control %>% 
+  distinct(Descriptors) %>% 
+  arrange(desc(Descriptors))
+
+
+                         
+output_folder <- "X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/stats_working/"
+
+write.csv(sites_merged_control, paste0(output_folder,"sites_control_merged",".csv"))                           
+  
+
+                        
+                              
+                                 
+                                 
+                                 
+                                
