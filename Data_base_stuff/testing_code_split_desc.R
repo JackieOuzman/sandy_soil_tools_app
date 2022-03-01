9bfa96916c24d73cadd05550a34f12a2f57112f4
Jackie Ouzman <jackie.ouzman@csiro.au>
2022-03-01 15:11:57 +1030

    got it:)

--- Data_base_stuff/testing_code_split_desc.R
+++ Data_base_stuff/testing_code_split_desc.R
@@ -1,4 +1,20 @@
 
+library(ggplot2)
+library(readxl)
+library(tidyverse)
+#library(multcompView)
+
+library(plotly)
+
+library(tidyverse)
+library(hrbrthemes)
+library(viridis)
+library(forcats)
+library(hrbrthemes)
+library(stringr)
+
+
+
 data_file <- "X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/stats_working/sites_merged.csv"
 data_file_control <- "X:/Therese_Jackie/Sandy_soils/Development_database/other_sites_working/stats_working/sites_control_merged.csv"
 
@@ -68,9 +84,14 @@ summary_control_data_all <- summary_control_data_all %>%
 
 
 ## how many different modification?
+#####################################################################################
+#### split the Descriptors into modification and amendments
+#####################################################################################
 
-#### split the Descriptors into modification
 
+#####################################################################################
+#####  soil modifications      #####
+#####################################################################################
 summary_control_data_all <- summary_control_data_all %>%
   distinct(site, Descriptors, .keep_all=TRUE) %>%
   select(site,
@@ -78,81 +99,68 @@ summary_control_data_all <- summary_control_data_all %>%
 summary_control_data_all <- summary_control_data_all %>% arrange(site,Descriptors )
 
 ### get the soil modification from the Descriptors
+
+#1 soil modification with depth
+summary_control_data_all <- summary_control_data_all %>% 
+  mutate(soil_modification_depth =  str_extract(summary_control_data_all$Descriptors, "[^_]+"))#keep everything before the first_
+
+#how many modification_depth
+soil_modification_depth <- summary_control_data_all %>% 
+  distinct(soil_modification_depth) %>% 
+  arrange(soil_modification_depth) %>% 
+  filter(soil_modification_depth != "Unmodified")
+  
+count(soil_modification_depth) #20 excluding the unmodified
+
+
+#1a working clms - soil modification without depth 1 only
 summary_control_data_all <- summary_control_data_all %>% 
   mutate(soil_modification_1 =  str_extract(summary_control_data_all$Descriptors, "[^.]+"))#keep everything before the first .
-head(summary_control_data_all)
+
 #the Unmodified seems to have a problem this fixes it :)
 summary_control_data_all <- summary_control_data_all %>% 
-  mutate(soil_modification_1a = str_extract(summary_control_data_all$soil_modification_1, "[^_]+")) #keep everything after the first _
+  mutate(soil_modification_1 = str_extract(summary_control_data_all$soil_modification_1, "[^_]+")) #keep everything after the first _
 
-#1, pull out everthing before first _
-
+#1a working clms - 2 soil modification without depth 2
 summary_control_data_all <- summary_control_data_all %>% 
-  mutate(soil_modification_1b =  str_extract(summary_control_data_all$Descriptors, "[^_]+"))#keep everything before the first_
+  mutate(soil_modification_2 = case_when(
+    soil_modification_depth == "Rip.50Spade.30" ~        "Rip+Spade",
+    soil_modification_depth == "Rip.60Spade.30" ~        "Rip+Spade",
+    soil_modification_depth == "Rip.45IncRip+Spade.30" ~ "IncRip+Spade",
+    soil_modification_depth == "Rip.60IncRip+Spade.30" ~ "IncRip+Spade",
+    TRUE ~ "NA"
+  ))
 
-# summary_control_data_all <- summary_control_data_all %>% 
-#   mutate(soil_modification_1text =  str_trunc(summary_control_data_all$soil_modification_1b, 
-#                                               6,
-#                                               side="left",
-#                                               ellipsis = "")) #keep 6 strings from the end
-# 
-# summary_control_data_all <- summary_control_data_all %>% 
-#   mutate(soil_modification_2text =  str_trunc(summary_control_data_all$soil_modification_1b, 
-#                                               8,
-#                                               side="left",
-#                                               ellipsis = "")) #keep 6 strings from the end
-# summary_control_data_all <- summary_control_data_all %>% 
-#   mutate(soil_modification_3text =  str_trunc(summary_control_data_all$soil_modification_2text, 
-#                                               5,
-#                                               side="right",
-#                                               ellipsis = "")) #keep 6 strings from the end  
-#          
-# head(summary_control_data_all)
+	
+#2 all soil modification without depth (if 2 soil modification used keep this)	
 
-
-
-
-
-
-
-
-
-
-
-
-
-### problem for later ...
 summary_control_data_all <- summary_control_data_all %>% 
   mutate(soil_modification = case_when(
-    Descriptors == "Rip.30IncRip_Gypsum.incorp_30" ~ "IncRip",
-    Descriptors == "Rip.30IncRip_none" ~ "IncRip",
-    Descriptors == "Rip.40IncRip_Lc.incorp_40" ~ "IncRip",
-    Descriptors == "Rip.40IncRip_none" ~ "IncRip",
-    Descriptors == "Rip.45IncRip_Fert_APP.band_45" ~ "IncRip",
-    Descriptors == "Rip.45IncRip_Fert_High.band_45" ~ "IncRip",
-    Descriptors == "Rip.45IncRip_Fert_Low.band_45" ~ "IncRip",
-    Descriptors == "Rip.45IncRip_none" ~ "IncRip",
-    Descriptors == "Rip.45IncRip_Fert.incorp_45" ~ "IncRip",
-    #Descriptors == "Rip.45IncRip+Spade.30_none" ~ "IncRip",
-    Descriptors == "Rip.50IncRip_Cl.surface" ~ "IncRip",
-    Descriptors == "Rip.50IncRip_none" ~ "IncRip",
-    Descriptors == "Rip.50IncRip_none" ~ "IncRip",
-    Descriptors == "Rip.50IncRip_none" ~ "IncRip",
-    Descriptors == "Rip.60IncRip_none" ~ "IncRip",
-    
-    Descriptors == "Rip.60Spade.30_none" ~ "Rip+Spade",
-    Descriptors == "Rip.50Spade.30_none" ~ "Rip+Spade",
-    Descriptors == "Rip.60Spade.30_none" ~ "Rip+Spade",
-    Descriptors == "Rip.45IncRip+Spade.30_none" ~ "Rip+Spade",
-    Descriptors == "Rip.60IncRip+Spade.30_none	" ~ "Rip+Spade",
-    Descriptors == "Rip.60Spade.30_none" ~ "Rip+Spade",
-    Descriptors == "Rip.60Spade.30_Lc.incorp_30+band_60	" ~ "Rip+Spade",
-    
-    Descriptors == "Rip.60IncRip+Spade.30_none" ~ "IncRip+Spade",
-    Descriptors == "Rip.45IncRip+Spade.30_none" ~ "IncRip+Spade",
-    TRUE ~ soil_modification))
+    soil_modification_2 == "NA" ~ soil_modification_1,
+    TRUE ~ soil_modification_2
+  ))
+	
+	
+### remove the working clms
+summary_control_data_all <- summary_control_data_all %>% 
+  select(-soil_modification_1,
+         -soil_modification_2)
 
 
+#how many modification without depth
+soil_modification_depth <- summary_control_data_all %>% 
+  distinct(soil_modification) %>% 
+  arrange(soil_modification) %>% 
+  filter(soil_modification != "Unmodified")
+
+count(soil_modification_depth) #7 excluding the unmodified 
+
+
+
+#####################################################################################
+#####  soil amendments      #####
+#####################################################################################
+back_up <- summary_control_data_all
 ### get the soil Amendment from the Descriptors
 # gets all the amendments
 summary_control_data_all <- summary_control_data_all %>% 
@@ -173,6 +181,19 @@ summary_control_data_all <- summary_control_data_all %>%
 summary_control_data_all <- summary_control_data_all %>% 
   mutate(amendment_2 = str_extract(summary_control_data_all$amendment_2b, "[^.]+"))
 
+### not quite right remove some problems!
+unique(summary_control_data_all$amendment_2)
+
+summary_control_data_all <- summary_control_data_all %>% 
+  mutate(amendment_2 = case_when(
+    amendment_2 ==  "surface_Yr18,19,20" ~ "",
+    amendment_2 ==  "incorp_50" ~ "",
+    amendment_2 ==  "band_30" ~ "",
+    amendment_2 ==  "band_50" ~ "",
+    amendment_2 ==  "surface" ~ "",
+    TRUE ~ amendment_2
+  ))
+unique(summary_control_data_all$amendment_2)
 
 
 
@@ -184,9 +205,7 @@ summary_control_data_all <- summary_control_data_all %>%
 summary_control_data_all <- summary_control_data_all %>% 
   mutate(amendment_3 = str_extract(summary_control_data_all$amendment_3b, "[^.]+"))
 
-#tidy up working outs
-summary_control_data_all <- summary_control_data_all %>%
-  select(-amendment_2a, -amendment_2b, -amendment_3a, -amendment_3b)
+
 
 
 
@@ -195,17 +214,37 @@ unique(summary_control_data_all$amendment_1)
 unique(summary_control_data_all$amendment_2)
 unique(summary_control_data_all$amendment_3)
 
-summary_control_data_all <- summary_control_data_all %>% 
-  mutate(amendment_2 = case_when(
-    amendment_2 ==  "surface_Yr18,19,20" ~ "NA",
-    amendment_2 ==  "incorp_50" ~ "NA",
-    amendment_2 ==  "band_30" ~ "NA",
-    amendment_2 ==  "band_50" ~ "NA",
-    amendment_2 ==  "surface" ~ "NA",
-    TRUE ~ amendment_2
-  ))
 
 
+## amendments 1, 2, 3 together.
+str(summary_control_data_all)
 
 
+summary_control_data_all$amendment_1 <-  
+  stringr::str_replace_na(summary_control_data_all$amendment_1 , replacement='')
 
+summary_control_data_all$amendment_2 <-  
+  stringr::str_replace_na(summary_control_data_all$amendment_2 , replacement='')  
+
+summary_control_data_all$amendment_3 <-  
+  stringr::str_replace_na(summary_control_data_all$amendment_3 , replacement='')
+
+
+summary_control_data_all <-  summary_control_data_all %>% 
+  mutate(amendment_123 = paste0(amendment_1,
+                                amendment_2,
+                                amendment_3))
+
+
+#tidy up working outs
+summary_control_data_all <- summary_control_data_all %>%
+  select(-amendment_2a, -amendment_2b, -amendment_3a, -amendment_3b)
+
+unique(summary_control_data_all$amendment_123)
+#how many amendment without depth
+amendment_no_depth <- summary_control_data_all %>% 
+  distinct(amendment_123) %>% 
+  arrange(amendment_123) %>% 
+  filter(amendment_123 != "none")
+
+count(amendment_no_depth) #26 excluding the none 
