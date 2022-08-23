10bb68c2cdfef8f5e879517fe4607acd554f6a20
Jackie Ouzman <jackie.ouzman@csiro.au>
2022-08-23 14:22:32 +0930

    add long names

--- Pre_app_working_2022/step5_pre_app_CumYld_ANOVA_tabel.R
+++ Pre_app_working_2022/step5_pre_app_CumYld_ANOVA_tabel.R
@@ -9,6 +9,10 @@ library(readxl)
 library(tidyverse)
 library(dplyr)
 
+
+list_of_Descriptors_with_order_rank <- read.csv("list_of_Descriptors_with_order_rank.csv")
+          
+
 ANOVA_Cum_Yld_df <- read.csv("X:/Therese_Jackie/Sandy_soils/Development_database/stats_batch_output/final_method/Cum_ANOVA_sites_merged_90.csv")
 ANOVA_Cum_Yld_df %>% 
   dplyr::distinct(site) #just amking sure we have Younghusband - we do:)
@@ -41,6 +45,10 @@ ANOVA_Cum_Yld_df <- ANOVA_Cum_Yld_df %>% separate(year, c("start","end"), "to", 
 
 #format the P value
 ANOVA_Cum_Yld_df$`p value` <- format(ANOVA_Cum_Yld_df$`p value`, digits = 4)
+
+# add the long name for desciptors
+ANOVA_Cum_Yld_df_test <- left_join(ANOVA_Cum_Yld_df, list_of_Descriptors_with_order_rank)
+  
 
 write.csv(ANOVA_Cum_Yld_df,
           "ANOVA_Cum_Yld_df_v2.csv",
