1122a887664570597feb67015b41336cb3f663f0
Ouzman <ouz001@csiro.au>
2022-10-24 10:45:09 +1030

    change ns anova to ns post hock

--- Data_base_stuff/stats/step 7 function for Cum ANOVA_P10 merge df.R
+++ Data_base_stuff/stats/step 7 function for Cum ANOVA_P10 merge df.R
@@ -1,12 +1,28 @@
 ### This is to merge Younghusband (AKA trouble) analysis with the other sites.
 
-
+library(ggplot2)
+library(readxl)
+library(tidyverse)
+library(dplyr)
 
 
 df1_Younghusband <- read.csv("X:/Therese_Jackie/Sandy_soils/Development_database/stats_batch_output/final_method/Cum_ANOVA_sites_merged_90 Younghusband.csv")
 df2_No_Younghusband <- read.csv("X:/Therese_Jackie/Sandy_soils/Development_database/stats_batch_output/final_method/Cum_ANOVA_sites_merged_90_no_Younghusband.csv")
 
 merge_Cum_ANOVA_sites_yr <- rbind(df1_Younghusband,df2_No_Younghusband)
+
+
+## now if ANOVA is ns then change the group LSD to ns
+names(merge_Cum_ANOVA_sites_yr)
+
+merge_Cum_ANOVA_sites_yr <- merge_Cum_ANOVA_sites_yr %>% 
+  mutate(
+    groups_LSD_cum = case_when(
+      ANOVA_sign_0.1 == "ns"   ~ "ns",
+      TRUE                      ~ "other"
+    )
+  )
+
 
 
 
